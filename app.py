e64353fce02cfc51187384e65efca6158d1db687
VISHAL M <ckvs16@gmail.com>
2026-09-11 09:49:17 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.title("AI-enabled Web Application")
+st.write("Welcome to the AI-enabled Web Application deployed using Streamlit Community
+Cloud.")
+32
+name = st.text_input("Enter your name")
+if st.button("Submit"):
+st.success(f"Hello, {name}! Your application is running successfully.")
